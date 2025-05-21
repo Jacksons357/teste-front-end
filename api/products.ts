4fcf01d0b695a2af40b5ee93ec84c830ca040301
Jacksons357 <jacksyntax20@gmail.com>
2025-05-21 16:47:43 -0300

    feat: add econverse project

--- api/products.ts
+++ api/products.ts
@@ -1,6 +1,9 @@
 export async function fetchProducts(): Promise<ProductsResponse> {
   try {
-    const response = await fetch('/api/products')
+    const API_URL = 'https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json'
+    const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/'
+    
+    const response = await fetch(CORS_PROXY + API_URL)
 
     if (!response.ok) {
       throw new Error('Erro ao buscar produtos')
