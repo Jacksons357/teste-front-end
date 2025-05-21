bf69e39858a113c545db25ce77517f95bbc177b7
Jacksons357 <jacksyntax20@gmail.com>
2025-05-21 16:30:33 -0300

    fix - error on return api

--- src/lib/api/products.ts
+++ src/lib/api/products.ts
@@ -1,6 +1,8 @@
 export async function fetchProducts(): Promise<ProductsResponse> {
   try {
-    const response = await fetch('/api/products')
+    const response = await fetch(
+      'https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json'
+    )
 
     if (!response.ok) {
       throw new Error('Erro ao buscar produtos')
